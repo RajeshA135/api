b56d751212d229d678b805e0102fa055c555c7bb
Rajesh Allam <rajeshallam9966@gmail.com>
2025-07-25 20:00:26 +0530

    Updated put, delete  methods in ProductAPIView

--- user/views/products.py
+++ user/views/products.py
@@ -37,6 +37,23 @@ class ProductAPIView(APIView):
             serializer.save()
             return Response({"data": serializer.data,"message":"Product Updated Successfully"})
         return Response(serializer.errors, status=400)
+    def put(self, request, pk):
+            product = get_object_or_404(Product, pk=pk, created_by=request.user)
+            serializer = ProductSerializer(product, data=request.data)
+            if serializer.is_valid():
+                serializer.save(created_by=request.user)  # Reassign user for security
+                return Response({
+                    "data": serializer.data,
+                    "message": "Entire Product details are updated successfully"
+                }, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        try:
+            product = Product.objects.get(pk=pk, created_by=request.user)
+        except Product.DoesNotExist:
+            return Response({"error": "Not found"}, status=404)
 
+        product.delete()
+        return Response({"message": "Deleted successfully"}, status=204)
     
     
