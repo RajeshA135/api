3935ff5f6c43e19864435d4da9c4b3d33249190e
Rajesh Allam <rajeshallam9966@gmail.com>
2025-08-04 18:58:06 +0530

    modified UserSerializer

--- user/serializers/users.py
+++ user/serializers/users.py
@@ -3,33 +3,39 @@ from ..models.roles import Role
 from rest_framework import serializers
 from .role import RoleSerializer
 class UserSerializer(serializers.ModelSerializer):
-        role = RoleSerializer(read_only=True)
-        email_name = serializers.CharField(read_only=True)           
-        email_domain = serializers.SerializerMethodField(read_only=True)
-        class Meta:
-            model = CustomUser
-            fields = ['id', 'username', 'email', 'password','is_staff', 'role', 'email_name', 'email_domain']
-            extra_kwargs = {'password': {'write_only': True}}
-            # depth = 1
-        
-        def get_email_name(self, obj):
-            if obj.email:
-                return obj.email.split('@')[0]
-            return None
-        def get_email_domain(self, obj):
-            if obj.email:
-              return obj.email.split('@')[1]
-            return None
-        def create(self, validated_data):
-            # Use Django's built-in method to hash passwords
-            user = CustomUser.objects.create_user(**validated_data)
-            return user
+    role = RoleSerializer(read_only=True)  # for displaying nested role details
+    role_id = serializers.PrimaryKeyRelatedField(
+        queryset=Role.objects.all(),
+        source='role',
+        write_only=True
+    )
+    email_name = serializers.SerializerMethodField(read_only=True)
+    email_domain = serializers.SerializerMethodField(read_only=True)
 
-        def update(self, instance, validated_data):
-            password = validated_data.pop('password', None)
-            for attr, value in validated_data.items():
-                setattr(instance, attr, value)
-            if password:
-                instance.set_password(password)
-            instance.save()
-            return instance
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password', 'is_staff', 'role', 'role_id', 'email_name', 'email_domain']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def get_email_name(self, obj):
+        if obj.email:
+            return obj.email.split('@')[0]
+        return None
+
+    def get_email_domain(self, obj):
+        if obj.email:
+            return obj.email.split('@')[1]
+        return None
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
